1412b94b69b8e35e346733c6121fb886562073da
HenryShaoR <178453040+HenryShaoR@users.noreply.github.com>
2024-11-02 19:52:37 +1100

    Add files via upload
    A file contains constants used to specify the target data set (i.e. users/articles/rooms) and the method of locating the data (by id or index)

---
+++ src/const.js
@@ -0,0 +1,20 @@
+export const By = {
+    id: true,
+    Id: true,
+    ID: true,
+    index: false,
+    Index: false,
+    INDEX: false
+};
+
+export const Target = {
+    u: 0,
+    user: 0,
+    users: 0,
+    a: 1,
+    article: 1,
+    articles: 1,
+    r: 2,
+    room: 2,
+    rooms: 2,
+};
