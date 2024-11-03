5f04955328dd91ddff6c6fb72859732c3c538d8f
HenryShaoR <178453040+HenryShaoR@users.noreply.github.com>
2024-11-03 15:20:44 +1100

    removed By, added Status

--- src/const.js
+++ src/const.js
@@ -1,11 +1,11 @@
-export const By = {
-    id: true,
-    Id: true,
-    ID: true,
-    index: false,
-    Index: false,
-    INDEX: false
-};
+// export const By = {
+//     id: true,
+//     Id: true,
+//     ID: true,
+//     index: false,
+//     Index: false,
+//     INDEX: false
+// };
 
 export const Target = {
     u: 0,
@@ -17,4 +17,15 @@ export const Target = {
     r: 2,
     room: 2,
     rooms: 2,
+};
+
+export const Status = {
+    deletedByUser: 0xf0000000,
+    deletedByAdmin: 0xf0000001,
+
+    authorOnly: 0x00000004, // for comments only: the comment is only visible to the author of the article.
+    friendsOnly: 0x00000003,
+    public: 0x00000002,
+    private: 0x00000001,
+    active: 0x00000000,
 };
