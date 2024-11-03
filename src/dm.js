7df011976068d5b19189965c2ddf007ea062e7ca
HenryShaoR <178453040+HenryShaoR@users.noreply.github.com>
2024-11-03 15:23:09 +1100

    adapt change in ChatRoom class and id logic

--- src/dm.js
+++ src/dm.js
@@ -5,22 +5,24 @@ import {
     ChatRoom,
     Messages
 } from "./data.js";
-import {By, Target} from "./const.js";
+import {Status, Target} from "./const.js";
 
 export function createRoom(uid) {
     updateData(new ChatRoom(
         newId(Target.room),
         uid,
+        Status.active,
         []
     ));
 }
 
 export function sendMessage(roomId, author, message) {
-    let room = getData(Target.room, By.id, roomId);
+    let room = getData(Target.room, roomId);
 
     if (author in room.uid) {
         room.message.push(new Messages(
             author,
+            null, // change in future to allow quote
             message,
             new Date()));
         updateData(room);
