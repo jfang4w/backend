96afd50e9ffea55bd07e3962f0d9058f112f9f92
HenryShaoR <178453040+HenryShaoR@users.noreply.github.com>
2024-11-07 15:46:09 +1100

    rename dm.js to room.js, adapt change in data structure

--- src/room.js
+++ src/room.js
@@ -2,25 +2,33 @@ import {
     newId,
     updateData,
     getData,
-    ChatRoom,
-    Messages
+    newRoom,
+    newMessage
 } from "./data.js";
 import {Status, Target} from "./const.js";
 
-export function createRoom(uid) {
-    updateData(new ChatRoom(
+export function createRoom(uid, roomName) {
+    updateData(newRoom(
         newId(Target.room),
         uid,
+        {},
+        roomName,
         Status.active,
         []
     ));
+}
+
+export function changeNickname(uid, roomId, nickname) {
+    const room = getData(Target.room, roomId);
+    room.nicknames[uid] = nickname;
+    updateData(room);
 }
 
 export function sendMessage(roomId, author, message) {
     let room = getData(Target.room, roomId);
 
     if (author in room.uid) {
-        room.message.push(new Messages(
+        room.message.push(newMessage(
             author,
             null, // change in future to allow quote
             message,
@@ -29,4 +37,8 @@ export function sendMessage(roomId, author, message) {
         return;
     }
     throw new Error(`uid ${author} should not be in this room`);
+}
+
+export function getRoom(roomId) {
+    return getData(Target.room, roomId);
 }
