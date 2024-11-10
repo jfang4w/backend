edc3c5c4df9da62cc93d0ef16f254abe149c4679
HenryShaoR <178453040+HenryShaoR@users.noreply.github.com>
2024-11-10 16:44:03 +1100

    more functions used
    include article and comment related functions

--- src/tests/client/index.js
+++ src/tests/client/index.js
@@ -189,3 +189,130 @@ async function remove(pwd, id, out) {
         out.innerText = data.error;
     }
 }
+
+async function upload(session, title, summary, content, long, price, tags, preId, out) {
+    const response = await fetch(`http://localhost:8800/v1/article/upload`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',  // Set the correct content type
+        },
+        body: JSON.stringify({
+            session: session,
+            title: title,
+            summary: summary,
+            content: content,
+            long: long,
+            price: price,
+            tags: tags,
+            preId: preId
+        })
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        out.style.color = 'green';
+        out.innerText = JSON.stringify(data);
+    } else {
+        out.style.color = 'red';
+        out.innerText = data.error;
+    }
+}
+
+async function articleDetails(articleId, out) {
+    const response = await fetch(`http://localhost:8800/v1/article/${articleId}`, {
+        method: 'GET'
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        out.style.color = 'green';
+        out.innerText = JSON.stringify(data);
+    } else {
+        out.style.color = 'red';
+        out.innerText = data.error;
+    }
+}
+
+async function articleUpdate(session, title, summary, content, rating, long, price, tags, articleId, preId, out) {
+    const response = await fetch(`http://localhost:8800/v1/article/${articleId}/update`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',  // Set the correct content type
+        },
+        body: JSON.stringify({
+            session: session,
+            title: title,
+            summary: summary,
+            content: content,
+            rating: rating,
+            long: long,
+            price: price,
+            tags: tags,
+            preId: preId
+        })
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        out.style.color = 'green';
+        out.innerText = JSON.stringify(data);
+    } else {
+        out.style.color = 'red';
+        out.innerText = data.error;
+    }
+}
+
+async function addComment(articleId, id, comment, out) {
+    const response = await fetch(`http://localhost:8800/v1/article/${articleId}/comment`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',  // Set the correct content type
+        },
+        body: JSON.stringify({
+            userId: id,
+            content: comment
+        })
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        out.style.color = 'green';
+        out.innerText = JSON.stringify(data);
+    } else {
+        out.style.color = 'red';
+        out.innerText = data.error;
+    }
+}
+
+async function addReply(parentIds, id, content, out) {
+    console.log({
+        parentIds: parentIds,
+        userId: id,
+        content: content
+    });
+    const response = await fetch(`http://localhost:8800/v1/comment/reply`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',  // Set the correct content type
+        },
+        body: JSON.stringify({
+            parentIds: parentIds,
+            userId: id,
+            content: content
+        })
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        out.style.color = 'green';
+        out.innerText = JSON.stringify(data);
+    } else {
+        out.style.color = 'red';
+        out.innerText = data.error;
+    }
+}
